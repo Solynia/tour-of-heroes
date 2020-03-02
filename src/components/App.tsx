2e42a464e1c490d096d9c88e7781f70142362745
Sébastien <sebastien.bouvier@accenture.com>
2020-03-02 19:02:31 +0100

    Adds navbar using plain css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Heroes from "./Heroes";
+import NavMenu from "./NavMenu";
 
 interface AppProps {
   title: string;
@@ -9,8 +10,11 @@ interface AppProps {
 function App(props: AppProps) {
   return (
     <div>
-      <h1>{props.title}</h1>
-      <Heroes />
+      <NavMenu></NavMenu>
+      <main>
+        <h1>{props.title}</h1>
+        <Heroes />
+      </main>
     </div>
   );
 }
