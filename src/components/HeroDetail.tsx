18dbf440674d92d5abbafa5b4caf9d1637ea5757
Sébastien <sebastien.bouvier@accenture.com>
2020-03-12 14:55:06 +0100

    fix heroDetail init state

--- src/components/HeroDetail.tsx
+++ src/components/HeroDetail.tsx
@@ -45,7 +45,7 @@ class HeroDetail extends React.Component<HeroDetailProps, HeroDetailState> {
 
   componentDidUpdate(prevProps: HeroDetailProps) {
     if (this.props.hero?.id !== prevProps.hero?.id) {
-      this.setState({ input: this.props.hero?.name });
+      this.setState({ input: this.props.hero?.name ?? "" });
     }
   }
 
